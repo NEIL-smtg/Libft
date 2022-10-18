db25f07e145334ecd9b96f3e6513c3e7342a37fb
NEIL-smtg <sunghui2000@hotmail.com>
2022-10-18 17:02:23 +0800

    lIBFT
    added Libft

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 19:00:26 by suchua            #+#    #+#             */
+/*   Updated: 2022/10/06 18:43:50 by suchua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int	size;
+	char			*tmp;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	size = 0;
+	tmp = (char *) malloc ((ft_strlen(s) + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	while (s[i])
+	{
+		tmp[i] = f(i, s[i]);
+		i++;
+	}
+	tmp[i] = 0;
+	return (tmp);
+}
