63d3453ed0edf4b5218f68dbe988f9b7a7288858
NEIL-smtg <sunghui2000@hotmail.com>
2023-04-01 07:39:20 +0800

    recode ft_split in recursive manner

--- srcs/ft_str/ft_split.c
+++ srcs/ft_str/ft_split.c
@@ -6,97 +6,41 @@
 /*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:33:34 by suchua            #+#    #+#             */
-/*   Updated: 2022/10/13 19:20:44 by suchua           ###   ########.fr       */
+/*   Updated: 2023/04/01 07:35:26 by suchua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	check_split(char me, char c)
+static char	**recursive_split(char const *s, char c, int depth)
 {
-	return (me == c);
-}
+	int		i;
+	char	*str;
+	char	**strs;
 
-static int	get_row_num(char const *s, char c)
-{
-	int	row;
-
-	row = 0;
-	while (*s)
-	{
-		while (check_split(*s, c) && *s)
-			s++;
-		if (!check_split(*s, c) && *s)
-			row++;
-		while (!check_split(*s, c) && *s)
-			s++;
-	}
-	return (row + 1);
-}
-
-static int	get_strlen(char const *s, char c, int cur_row)
-{
-	int	row;
-	int	len;
-
-	row = 0;
-	len = 0;
-	while (row < cur_row)
-	{
-		while (check_split(*s, c) && *s)
-			s++;
-		if (!check_split(*s, c) && *s)
-			row++;
-		while (!check_split(*s, c) && *s)
-		{
-			if (row == cur_row)
-				len++;
-			s++;
-		}
-	}
-	return (len + 1);
-}
-
-static void	save_str(char *str, char const *s, char c, int cur_row)
-{
-	int	row;
-	int	len;
-
-	row = 0;
-	len = 0;
-	while (row < cur_row)
-	{
-		while (check_split(*s, c) && *s)
-			s++;
-		if (!check_split(*s, c) && *s)
-			row++;
-		while (!check_split(*s, c) && *s)
-		{
-			if (row == cur_row)
-				str[len++] = *s;
-			s++;
-		}
-		str[len] = '\0';
-	}
+	i = 0;
+	while (s[i] != c)
+		++i;
+	str = ft_substr(s, 0, i);
+	while (s[i] == c)
+		++i;
+	if (s[i])
+		strs = recursive_split(s + i, c, depth + 1);
+	else
+		strs = ft_calloc(depth + 2, sizeof(char *));
+	strs[depth] = str;
+	return (strs);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
-	char	**new_arr;
-
 	if (!s)
 		return (NULL);
-	i = 0;
-	new_arr = (char **) malloc (get_row_num(s, c) * sizeof(char *));
-	while (i < get_row_num(s, c) - 1)
-	{
-		new_arr[i] = (char *) malloc (get_strlen(s, c, (i + 1)) * sizeof(char));
-		save_str(new_arr[i], s, c, (i + 1));
-		i++;
-	}
-	new_arr[i] = 0;
-	return (new_arr);
+	while (*s == c)
+		s++;
+	if (!s)
+		return (ft_calloc(1, sizeof(char *)));
+	return (recursive_split(s, c, 0));
 }
 
 // int main(void)
@@ -104,18 +48,12 @@ char	**ft_split(char const *s, char c)
 // 	char const str[] = "      split       this for   me  !       ";
 // 	char c = (char)' ';
 // 	char **p = ft_split(str, c);
-// 	int i = 0;
-
-// 	while (i < get_row_num(str, c))
+// 	int	i = -1;
+// 	while (p[++i])
 // 	{
-// 		printf("%s\n", p[i]);
-// 		i++;
-// 	}
-// 	i = 0;
-// 	while (i < get_row_num(str, c) - 1)
-// 	{
+// 		ft_printf("%s\n", p[i]);
 // 		free(p[i]);
-// 		i++;
 // 	}
+// 	free(p);
 // 	return (0);
 // }
